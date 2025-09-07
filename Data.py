ce2a7f7ef85ca802bbc1b6b48ccf157438796a80
Lakshit Sharma <pythondevloper78@outlook.com>
2025-09-07 13:54:50 +0530

    Game libraraies
    The logo file includes library and Data.py file include data of challengers

---
+++ Data.py
@@ -0,0 +1,68 @@
+Game_data = [
+    {
+        "name" : "Cristiano Ronaldo",
+        "Followers_count" : 630,
+        "Country" : "Portugal",
+        "Hobbies" : "Footballer"
+},
+    {
+        "name" : "Lionel Messi",
+        "Followers_count" : 500,
+        "Country" : "Argentina",
+        "Hobbies" : "Footballer"
+    },
+    {
+        "name" : "Taylor Swift",
+        "Followers_count" : 450,
+        "Country" : "USA",
+        "Hobbies" : "Singer"
+    },
+    {
+        "name": "Selena Gomez",
+        "Followers_count": 430,
+        "Country": "USA",
+        "Hobbies": "Singer"
+    },
+    {
+        "name": "Dwayne Johnson",
+        "Followers_count": 390,
+        "Country": "USA",
+        "Hobbies": "Actor"
+    },
+    {
+        "name": "Kim Kardashian",
+        "Followers_count": 360,
+        "Country": "USA",
+        "Hobbies": "Media personality"
+    },
+    {
+        "name": "Neymar Jr.",
+        "Followers_count": 360,
+        "Country": "Brazil",
+        "Hobbies": "Footballer"
+    },
+    {
+        "name": "Virat Kohli",
+        "Followers_count": 270,
+        "Country": "India",
+        "Hobbies": "Cricketer"
+    },
+    {
+        "name": "Virat Kohli",
+        "Followers_count": 270,
+        "Country": "India",
+        "Hobbies": "Cricketer"
+    },
+    {
+        "name": "Narendra Modi ji",
+        "Followers_count": 250,
+        "Country": "India",
+        "Hobbies": "Prime Minister"
+    },
+    {
+        "name": "B.T.S",
+        "Followers_count": 100,
+        "Country": "South Korea",
+        "Hobbies": "K-pop Artist"
+    }
+]
